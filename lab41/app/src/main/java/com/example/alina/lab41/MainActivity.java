2a5bf2a89a1c900593b1e015fa1f298cf59cce72
oleynik.a.o <oleynik.a.o@gmail.com>
2018-05-27 04:59:10 +0300

    lab4 new

--- lab41/app/src/main/java/com/example/alina/lab41/MainActivity.java
+++ lab41/app/src/main/java/com/example/alina/lab41/MainActivity.java
@@ -1,37 +1,150 @@
 package com.example.alina.lab41;
 
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
-    VideoView videoPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity implements OnPreparedListener,
+        OnCompletionListener {
+    final String LOG_TAG = "myLogs";
+
+    final String DATA_HTTP = "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3";
+    final String DATA_STREAM = "http://online.radiorecord.ru:8101/rr_128";
+    final String DATA_SD = Environment
+            .getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
+            + "/kaleo.mp3";
+
+
+    MediaPlayer mediaPlayer;
+    AudioManager am;
+    CheckBox chbLoop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoPlayer =  (VideoView)findViewById(R.id.videoPlayer);
-        Uri myVideoUri= Uri.parse( "android.resource://" + getPackageName() + "/" + R.raw.gu);
-        videoPlayer.setVideoURI(myVideoUri);
-        MediaController mediaController = new MediaController(this);
-        videoPlayer.setMediaController(mediaController);
-        mediaController.setMediaPlayer(videoPlayer);
+        am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        chbLoop = (CheckBox) findViewById(R.id.chbLoop);
+        chbLoop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (mediaPlayer != null)
+                    mediaPlayer.setLooping(isChecked);
+            }
+        });
     }
 
-    public void play(View view){
-        videoPlayer.start();
-    }
-    public void pause(View view){
+    public void onClickStart(View view) {
+        releaseMP();
 
-        videoPlayer.pause();
+        try {
+            switch (view.getId()) {
+                case R.id.btnStartHttp:
+                    Log.d(LOG_TAG, "start HTTP");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_HTTP);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartStream:
+                    Log.d(LOG_TAG, "start Stream");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_STREAM);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    Log.d(LOG_TAG, "prepareAsync");
+                    mediaPlayer.setOnPreparedListener(this);
+                    mediaPlayer.prepareAsync();
+                    break;
+                case R.id.btnStartSD:
+                    Log.d(LOG_TAG, "start SD");
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(DATA_SD);
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    break;
+                case R.id.btnStartRaw:
+                    Log.d(LOG_TAG, "start Raw");
+                    mediaPlayer = MediaPlayer.create(this, R.raw.kaleo);
+                    mediaPlayer.start();
+                    break;
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (mediaPlayer == null)
+            return;
+
+        mediaPlayer.setLooping(chbLoop.isChecked());
+        mediaPlayer.setOnCompletionListener(this);
     }
-    public void stop(View view){
-        videoPlayer.stopPlayback();
-        videoPlayer.resume();
+
+    private void releaseMP() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.release();
+                mediaPlayer = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        if (mediaPlayer == null)
+            return;
+        switch (view.getId()) {
+            case R.id.btnPause:
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                break;
+            case R.id.btnResume:
+                if (!mediaPlayer.isPlaying())
+                    mediaPlayer.start();
+                break;
+            case R.id.btnStop:
+                mediaPlayer.stop();
+                break;
+
+
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onPrepared");
+        mp.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.d(LOG_TAG, "onCompletion");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMP();
     }
 }
